bd23098c96abff90dd33c17bcc024e255fa3fa40
RedBren72 <brendanweir13@gmail.com>
2026-01-18 21:21:14 +0000

    Changes to be committed: 	modified:   pyGame.py

--- pyGame.py
+++ pyGame.py
@@ -7,13 +7,46 @@ import sys
 
 pygame.init()
 
-screen_area = width, height = 640, 480
+# Constants for the original game's screen/grid size
+# ZX Spectrum screen is 32 columns x 24 rows (approx)
 
-print(type(screen_area))
+# Colour Definitions
+rgbBLACK = (0, 0, 0)
+rgbRED = (200, 0, 0)
+rgbGREEN = (0, 200, 0)
+rgbBLUE = (0, 0, 200)
+rgbYELLOW = (200, 200, 0)
+rgbCYAN = (0, 200, 200)
+rgbMAGENTA = (200, 0, 200)
+rgbWHITE = (255, 255, 255)
 
-screen = pygame.display.set_mode( screen_area )
+# Screen dimensions
 
-print( type( screen ) )
+scrAREA = scrWIDTH, scrHEIGHT = 640, 480
+scrSIZE = 20
+
+print(type(scrAREA))
+
+gameDisplay = pygame.display
+gameScreen = gameDisplay.set_mode( scrAREA )
+
+gameDisplay.set_caption( "Thru' The Wall" )
+
+print( type( gameScreen ) )
+
+# Outer loop
+gameExit = False
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+
+    # Inner loop
+
+
+    gameScreen.fill( rgbBLACK )
+    pygame.display.update()
+    
 
 print( "Exiting..." )
 pygame.quit()
